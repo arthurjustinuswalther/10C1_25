8d6c919ec0b7bba567a45fa6fdf0d736bdba3bb4
KOEZIGSKOBR4 <insertemail@here.com>
2026-03-13 20:45:39 +0100

    umbennenung und einbindung der tangram steine

--- src/main/java/czg/objects/minigame_objects/TangramPieceObject.java
+++ src/main/java/czg/objects/minigame_objects/TangramPieceObject.java
@@ -4,10 +4,22 @@
  */
 package czg.objects.minigame_objects;
 
-/**
- *
- * @author guest-hlixvk
- */
-public enum TangramPieceObject {
+import czg.util.Images;
 
+import java.awt.*;
+
+public enum TangramPieceObject {
+    TRIANGLE_BIG_1("/assets/minigames/mathematics/tangram_piece_00.png"),
+    TRIANGLE_BIG_2("/assets/minigames/mathematics/tangram_piece_01.png"),
+    TRIANGLE_MEDIUM("/assets/minigames/mathematics/tangram_piece_02.png"),
+    TRIANGLE_SMALL_1("/assets/minigames/mathematics/tangram_piece_03.png"),
+    TRIANGLE_SMALL_2("/assets/minigames/mathematics/tangram_piece_04.png"),
+    PARALLELOGRAM("/assets/minigames/mathematics/tangram_piece_05.png"),
+    SQUARE("/assets/minigames/mathematics/tangram_piece_06.png");
+
+    public final Image sprite;
+
+   TangramPieceObject(String path) {
+        this.sprite = Images.get(path);
+    }
 }
